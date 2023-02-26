9834d5502dc3a052fbc23dea92e5ec79ff4ad9eb
younesdakka <111289126+younesdakka@users.noreply.github.com>
2023-02-26 13:18:53 +0300

    j
    h

---
+++ src/app/hamad-hospital/hamad-hospital.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, distinctUntilChanged, map } from 'rxjs';
+import { getFormApiGonfig } from '../shared/models';
+import { AuthService } from '../shared/services/auth.service';
+import { GetFormApiService } from '../shared/services/functionsForHandelWithApi/getFormApi.service';
+import { HttpHRService } from '../shared/services/httpHR.service';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class HamadHospitalService {
+    constructor(
+        private _http: HttpHRService,
+        private _getFormApiService: GetFormApiService,
+        private _authService: AuthService
+    ) {}
+    store = new BehaviorSubject<jobFunctionalModel>({
+        codes: undefined,
+        needsTable: { data: [], loading: false },
+        filterForNeedsTable: undefined,
+    });
+    store$ = this.store.asObservable();
+    updateStore(newSate: jobFunctionalModel) {
+        this.store.next({
+            ...this.store.value,
+            ...newSate,
+        });
+    }
+    get dataStore() {
+        return this.store.getValue();
+    }
+
+    Selector$(selector: selectorsType) {
+        return this.store$.pipe(
+            map((value: any) => value[selector]),
+            distinctUntilChanged()
+        );
+    }
+    saveData(data: any) {
+        return this._http.saveData('HeaderSlider/HeaderSliderSave', data);
+    }
+
+    getFormApi(
+        api: string,
+        selector: selectorsType,
+        params?: any,
+        config?: getFormApiGonfig
+    ) {
+        this._getFormApiService.getFormApi(
+            this.store,
+            api,
+            selector,
+            params,
+            config
+        );
+    }
+}
+export interface jobFunctionalModel {
+    codes?: any;
+    needsTable?: { data: any; loading: boolean };
+    filterForNeedsTable?: any;
+}
+export type selectorsType = keyof jobFunctionalModel;
--- src/app/hamad-hospital/home/home.component.ts
+++ src/app/hamad-hospital/home/home.component.ts
@@ -1,34 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { MessageService } from 'primeng/api';
+import { HamadHospitalService } from '../hamad-hospital.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent  implements OnInit {
+export class HomeComponent implements OnInit {
+    formSlider: FormGroup;
+    FormData: FormGroup;
 
-    formSlider:FormGroup;
-
-    constructor(fb:FormBuilder) {
+    constructor(
+        fb: FormBuilder,
+        private _HamadHospitalService: HamadHospitalService,
+        private messageService: MessageService,
+        private _translateService: TranslateService
+    ) {
         this.formSlider = fb.group({
-            ID:[''],
-            Image:[''],
-            TitleAr:[''],
-            TitleEn:[''],
-            Sorting:[''],
-            IsActive:[''],
-        })
-     }
-
-    ngOnInit() {
+            ID: [''],
+            Image: [''],
+            TitleAr: [''],
+            TitleEn: [''],
+            Sorting: [''],
+            IsActive: [''],
+        });
+        this.FormData = fb.group({
+            MedicalRehabilitationIconPath: [null],
+            MedicalRehabilitationIconDescAr: [null],
+            MedicalRehabilitationIconDescEn: [null],
+            ProstheticsIconPath: [null],
+            ProstheticsDescAr: [null],
+            ProstheticsDescEn: [null],
+            HearingIconPath: [null],
+            HearingDescAr: [null],
+            HearingDescEn: [null],
+            OutpatientClinicsIconPath: [null],
+            OutpatientClinicsDescAr: [null],
+            OutpatientClinicsDescEn: [null],
+        });
     }
 
-    save(){
-   console.log(this.formSlider.value , "gegegegeg");
+    ngOnInit() {}
+
+    save() {
+        // this._HamadHospitalService.saveData(this.formSlider.value);
+        if (this.formSlider.invalid) {
+            this.messageService.add({
+                severity: 'error',
+                detail: this._translateService.instant(
+                    'Shared.THERE_ARE_REQUIRED_FIELDS'
+                ),
+            });
+        } else {
+            console.log(this.formSlider.value, 'fffpeoeoeoeo');
+            this._HamadHospitalService.saveData(this.formSlider.value);
+        }
+        console.log(this.formSlider.value, 'gegegegeg');
     }
-
-        checked: boolean | undefined;
-
-
+    clear() {}
+    checked: boolean | undefined;
 }
